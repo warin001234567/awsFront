20f6f076969c3d207958be0a4b7974bdcafb2df4
pittawas17 <60070064@kmitl.ac.th>
2020-05-23 00:22:41 +0700

    Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,113 +1,60 @@
-import Link from 'next/link'
-import Head from 'next/head'
-import {Nav, Form} from 'react-bootstrap'
+import React, { Component, useRef, useLayoutEffect } from "react";
+import Link from "next/link";
+import Head from "next/head";
+import { Nav, Form, FormControl } from "react-bootstrap";
 
 const Navbar = () => (
-    <div>
-        <Nav className="navbar navbar-expand-lg justify-content-between" style={{position: 'fixed', width: '100%', zIndex: 10}}>
-        <Link href="./">
-            <a class="navbar-brand m-0 pl-3">
-                <img id="logo" src="../static/logos/logo.png" style={{width: '60%'}}/>
-            </a>
-        </Link>
-      
-      {/* <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-5 ml-auto pr-5">
-
-
-          <li class="nav-item mr-3 invisible-fix">
-              <Link>
-            <a onclick="extendTheater()" class="select selecter nav-link" style="
-                  border-bottom: 1px solid white;
-                  width: 100%;
-                  text-align: center;
-                  color: white;
-                ">
-              <span id="theatername">Select Theater</span>
-              <i id="theaterpoint" class="ml-3 fas fa-caret-down"></i>
-            </a>
-            </Link>
-          </li>
-          <li class="nav-item invisible-fix mr-3">
-              <Link>
-            <a onclick="extendMovie()" class="select selecter nav-link" style="
-                  border-bottom: 1px solid white;
-                  width: 100%;
-                  text-align: center;
-                  color: white;
-                ">
-              <span id="moviename">Select Movie</span>
-              <i id="moviepoint" class="ml-3 fas fa-caret-down"></i>
-            </a>
-            </Link>
-          </li>
-          <li class="nav-item invisible-fix mt-2" style="height: 28%;">
-            <a href="" id="" class="select nav-link p-0 mybtn" type="button" data-toggle="collapse"
-              data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
-              aria-expanded="false" aria-label="Toggle navigation">
-                  <Link>
-              <a onclick="bookingNav()" class="select nav-link third text-white" style="height: 30px; padding-top: 0;">
-                Go!</a>
-                </Link>
-            </a>
-          </li>
-        </ul>*/}
-        
-
-
-        <Form inline className=" my-2 my-lg-0" id="login-cont">
-            <a id="signinbtn" class="mr-4 text-white"
-              onclick="document.getElementById('modal-wrapper').style.display='block',signin()"
-              style="cursor: pointer;">SIGN IN</a>
-            <a id="signupbtn" class="mr-3 text-white"
-              onclick="document.getElementById('modal-wrapper').style.display='block',signup()"
-              style="cursor: pointer;">SIGN UP</a>
-            <input class="mr-2 myform pl-2 pb-1" type="search" aria-label="Search"
-              style="height: 5%; width: 13vw; border-radius: 45px / 45px;" />
-            <i class="fa fa-search fa-sm m-0" style="color: white; cursor: pointer;" aria-hidden="true"></i>
-        </Form>
+  <div>
+    <Nav
+      className={
+        "solid-nav pt-3 navbar navbar-expand-lg justify-content-between"
+      }
+      style={{ position: "fixed", width: "100%", zIndex: 10 }}
+    >
+      <Link href="./">
+        <a class="navbar-brand m-0 pl-3">
+          <img
+            id="logo"
+            src="../static/logos/logo.png"
+            style={{ width: "60%" }}
+          />
+        </a>
+      </Link>
+      <Form inline className=" my-2 my-lg-0" id="login-cont">
+        <a
+          id="signinbtn"
+          className="mr-4 text-white"
+          onClick="document.getElementById('modal-wrapper').style.display='block',signin()"
+          style={{ cursor: "pointer" }}
+        >
+          SIGN IN
+        </a>
+        <a
+          id="signupbtn"
+          className="mr-3 text-white"
+          onClick="document.getElementById('modal-wrapper').style.display='block',signup()"
+          style={{ cursor: "pointer" }}
+        >
+          SIGN UP
+        </a>
+        <FormControl
+          type="text"
+          aria-label="Search"
+          className="mr-2 myform pl-2 pb-1"
+          style={{
+            height: "5%",
+            width: "13vw",
+            borderRadius: "45px / 45px",
+          }}
+        />
+        <i
+          className="fa fa-search fa-sm m-0"
+          style={{ color: "white", cursor: "pointer" }}
+          aria-hidden="true"
+        ></i>
+      </Form>
     </Nav>
-    
-    {/* <div id="extendMovie" class="invisible-fix" style="
-          width: 100%;
-          height: 450px;
-          background-color: black;
-          position: fixed;
-          z-index: 9;
-        ">
-      <div class="container-fluid pt-5 pl-5 pr-5 pb-0" id="selectMovie">
-        <h2 style="position: fixed; color: white;">Movie</h2>
-        <div class="row flex-row flex-nowrap pl-3 pt-5">
-          <div v-for="blog in blogSearchResult">
-            <div class="card my-1 pt-2 text-white text-center" onclick="setMovie(this)" style="
-                  height: 95%;
-                  width: 10vw;
-                  margin-right: 2vw;
-                  cursor: pointer;
-                  background-color: transparent;
-                ">
-              <img src="blog.image" class="card-img-top img-fluid" alt="..." style="
-                    height: auto;
-                    width: 10vw;
-                    object-fit: contain;
-                    margin-top: 0%;
-                  " /> */}
-              {/* <div class="card-body" v-bind:class="{'bg-dark text-white': blog.color}"
-                style="text-align: left !important;">
-                <p id="movietitle" class="card-title" style="font-size: 1.2vw; margin-top: -0.5vw;">
-                  <% blog.title %>
-                </p>
-                <p class="card-text2"><% blog.thtitle %><br /></p>
-              </div> */}
-            {/* </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <style jsx>{`
-        
-      `}</style> */}
-    </div>
-)
+  </div>
+);
 
-export default Navbar
+export default Navbar;
