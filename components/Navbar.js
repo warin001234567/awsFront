88e0a7b75d564ef527d3bbb5b333f8154563d121
pittawas17 <60070064@kmitl.ac.th>
2020-05-23 00:47:48 +0700

    toggle sign in

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,9 +9,7 @@ const Navbar = (props) => {
 
   const [modal, setModal] = useState(false);
 
-  const toggleSingIn = () => setModal(!modal);
-
-  const toggleSingUp = () => setModal(!modal);
+  const toggleSignIn = () => setModal(!modal);
 
   return (
     <div>
@@ -37,12 +35,13 @@ const Navbar = (props) => {
             onClick={toggleSignIn}
             style={{ cursor: "pointer" }}
           >
+            {" "}
             SIGN IN
           </a>
           <a
             id="signupbtn"
             className="mr-3 text-white"
-            onClick={toggleSignUp}
+            // onClick={toggleSignUp}
             style={{ cursor: "pointer" }}
           >
             SIGN UP
@@ -64,6 +63,26 @@ const Navbar = (props) => {
           ></i>
         </Form>
       </Nav>
+      <Modal isOpen={modal} toggle={toggleSignIn} className={className}>
+        <ModalHeader toggle={toggleSignIn}>Modal title</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggleSignIn}>
+            Do Something
+          </Button>{" "}
+          <Button color="secondary" onClick={toggleSignIn}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 };
